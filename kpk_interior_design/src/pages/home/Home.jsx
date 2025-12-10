d9b7953999a4c1fe2642d1c7e422f604d782c233
unknown <Bharath-code99>
2025-12-10 19:03:06 +0530

    home page

--- kpk_interior_design/src/pages/home/Home.jsx
+++ kpk_interior_design/src/pages/home/Home.jsx
@@ -1,11 +1,18 @@
+import DesignProcess from "../services/designprocess/DesignProcess";
 import Faq from "../services/faq/Faq";
 import HappyClients from "../services/happyclient/HappyClients";
+import CraftingSpace from "./craftingspace/CraftingSpace";
+import CreativeDesign from "./creativedesign/CreativeDesign";
+import DesignSession from "./designsession/DesignSession";
 import HomeAboutUs from "./homeaboutus/HomeAboutUs";
 import HomeHeroSec from "./homehero/HomeHeroSec";
 import HomeOurService from "./homeourservice/HomeOurService";
 import HomeRankSec from "./homeranksec/HomeRankSec";
 import HomeRateSec from "./homeratesec/HomeRateSec";
 import HomeRatingSec from "./homerating/HomeRatingSec";
+import TopCategory from "./topcategory/TopCategory";
+import WhyChooseUs from "./whychooseus/whychooseus";
+import Workflow from "./workflow/Workflow";
 
 
 export default function Home(){
@@ -17,6 +24,12 @@ export default function Home(){
     <HomeOurService/>
     <HomeAboutUs/>
     <HomeRateSec/>
+    <WhyChooseUs/>
+   <CraftingSpace/>
+    <Workflow/>
+    <DesignSession/>
+    <CreativeDesign/>
+    <TopCategory/>
     <HappyClients/>
      <Faq/>
         </>
---
+++ kpk_interior_design/src/pages/home/designsession/DesignSession.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import './DesignSession.css'
+import office from '../../../assets/home/images/Frame 1000012852.png'
+import home from '../../../assets/home/images/Frame 1000012853.png'
+import laptop from '../../../assets/home/images/Frame 1000012854.png'
+import tablet from '../../../assets/home/images/Frame 1000012855.png'
+
+function DesignSession(){
+   const steps = [{
+      id:1,
+      img: office,
+      title:"Step 1: Meet Our Designer",
+    },
+    {
+        id:2,
+      img: home,
+      title: "Step 2: Walkthrough our Studio",
+    },
+    {
+        id:3,
+      img: laptop,
+      title: "Step 3: Free Personalized 3D Designs",
+    },
+    {
+        id:4,
+      img: tablet,
+      title: "Step 4: Get An Instant Quote",
+    }]
+    return(
+        <>
+            <div className="designsession_container2">
+                <div className="designsession_text2">
+                    <p className="design_badge2">Design Session</p>
+                    <h2 className="designsession_title2">Personalized Interior 
+                        Design Consultations by Professional Designers in Pondicherry 
+                    </h2>
+                    <p className="designsession_des2">
+                        KPK Enterprise, a top interior design brand in Pondicherry, 
+                        delivers tailored consultations. We help homeowners and businesses 
+                        plan modular kitchens, wardrobes, TV units, pooja units, and study 
+                        tables with precision, creativity, and lasting quality.
+                    </p>
+                </div>
+                <div className="steps_sectiongrid2">
+                    {steps.map(data=>(
+                        <div className="steps_card2" key={data.id}>
+                            <img src={data.img} alt="" className="sesscard_img2" />
+                            <p className="sesscard_title2">{data.title}</p>
+                        </div>
+                    ))}
+                </div>
+                <div className="session_btnwrapper2">
+                    <button className="session_btn2">Connect With Design Experts</button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default DesignSession
